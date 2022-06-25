4d39e0c8203989cfcb2074eafe2194c42cb2cbf4
luna <lunagibson@duck.com>
2022-06-25 15:12:50 +0100

    Initial commit

---
+++ src/main/java/me/lunaluna/fabric/kotlinconfig/KotlinConfig.java
@@ -0,0 +1,10 @@
+package me.lunaluna.fabric.kotlinconfig;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KotlinConfig implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/lunaluna/fabric/kotlinconfig/client/KotlinConfigClient.java
@@ -0,0 +1,13 @@
+package me.lunaluna.fabric.kotlinconfig.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class KotlinConfigClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
